3ea319c85f548f99d92fc7ca459a3e2a4117be97
Daniel Nicodemo <dan@showcaseidx.com>
2018-10-25 13:41:43 -0400

    If Else statements to generate logs BORING

--- raceDay.js
+++ raceDay.js
@@ -1,15 +1,18 @@
-const registeredEarly = true;
-const runnerAge = 19;
+const registeredEarly = false;
+const runnerAge = 12;
 
 let raceNumber = Math.floor(Math.random() * 1000);
-//earlyAdults >= 1000
-//lateAdults =< 1000
-//adultStart = '9:30am' || '11:00am'
-//youthStart = '12:30'
+
 if (runnerAge > 18 && registeredEarly) {
   raceNumber += 1000;
 }
 
 if (runnerAge > 18 && registeredEarly) {
   console.log('You race time begins at 9:30AM and your number is ' + raceNumber);
+} else if (runnerAge > 18 && registeredEarly === false) {
+  console.log('Late adults run at 11:00 am.  Your race number is: ' + raceNumber);
+} else if (runnerAge < 18) {
+  console.log('Youth run at 12:30pm. Your race number is: ' + raceNumber);
+} else {
+  console.log('Please see the registration desk');
 }
